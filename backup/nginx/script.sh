9e1cea82d0acc5337a0439cd0ef960238349a8d5
gowdasagar06 <sagargowda6666@gmail.com>
2024-02-01 15:36:05 +0000

    push

---
+++ backup/nginx/script.sh
@@ -0,0 +1,3 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx  --namespace ingress-nginx --create-namespace \
+  --set-string controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb"
